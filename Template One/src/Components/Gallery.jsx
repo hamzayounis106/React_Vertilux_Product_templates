3ab2637a338ef03d03469da79fa493c1b5788608
Aqibmalik57 <aqibmalik1586@gmail.com>
2025-02-05 12:13:24 +0500

    adding and removing the cursor pointer on images

--- Template One/src/Components/Gallery.jsx
+++ Template One/src/Components/Gallery.jsx
@@ -67,7 +67,7 @@ export default function Gallery() {
                 <img
                   src={image}
                   alt={`Thumbnail ${index + 1}`}
-                  className='w-full h-auto rounded-md cursor-pointer'
+                  className='w-full h-auto rounded-md'
                 />
                 <div className='absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-md cursor-pointer'>
                   <span className='text-white text-lg font-semibold'>
